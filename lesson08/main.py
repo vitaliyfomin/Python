1f58730223f1e45fee98ef72c2d8e5a7e1404841
Vitaliy Fomin <fomin_v.v@mail.ru>
2023-12-07 19:03:28 +0500

    добавил копирование данных из одного файла в другой

--- lesson08/main.py
+++ lesson08/main.py
@@ -1,66 +1,17 @@
-from os.path import exists
-from csv import DictReader, DictWriter
+def copy_entry(source_file, destination_file):
+    try:
+        entry_number = int(input("Введите номер строки для копирования: ")) - 1  # Строки начинаются с 1, а индексы с 0
+        source_data = read_file(source_file)
 
-class LenNumberError(Exception):
-    def __init__(self, txt):
-        self.txt = txt
+        if 0 <= entry_number < len(source_data):
+            entry_to_copy = source_data[entry_number]
+            write_file(destination_file, [entry_to_copy["Имя"], entry_to_copy["Фамилия"], entry_to_copy["Телефон"]])
+            print("Данные успешно скопированы.")
+        else:
+            print("Неверный номер строки.")
 
-class NameError(Exception):
-    def __init__(self, txt):
-        self.txt = txt
-
-def get_info():
-    is_valid_first_name = False
-    while not is_valid_first_name:
-        try:
-            first_name = input("Введите имя: ")
-            if len(first_name) < 2:
-                raise NameError("Не валидное имя")
-            else:
-                is_valid_first_name = True
-        except NameError as err:
-            print(err)
-
-    last_name = "Иванов"
-
-    is_valid_phone = False
-    while not is_valid_phone:
-        try:
-            phone_number = int(input("Введите номер: "))
-            if len(str(phone_number)) != 11:
-                raise LenNumberError("Неверная длина номера")
-            else:
-                is_valid_phone = True
-        except ValueError:
-            print("Не валидный номер!!!")
-        except LenNumberError as err:
-            print(err)
-
-    return [first_name, last_name, phone_number]
-
-def create_file(file_name):
-    with open(file_name, "w", encoding='utf-8') as data:
-        f_writer = DictWriter(data, fieldnames=['Имя', 'Фамилия', 'Телефон'])
-        f_writer.writeheader()
-
-def read_file(file_name):
-    with open(file_name, "r", encoding='utf-8') as data:
-        f_reader = DictReader(data)
-        return list(f_reader)
-
-def write_file(file_name, lst):
-    res = read_file(file_name)
-    for el in res:
-        if el["Телефон"] == str(lst[2]):
-            print("Такой телефон уже есть")
-            return
-
-    obj = {"Имя": lst[0], "Фамилия": lst[1], "Телефон": lst[2]}
-    res.append(obj)
-    with open(file_name, "w", encoding='utf-8', newline='') as data:
-        f_writer = DictWriter(data, fieldnames=['Имя', 'Фамилия', 'Телефон'])
-        f_writer.writeheader()
-        f_writer.writerows(res)
+    except ValueError:
+        print("Введите корректный номер строки.")
 
 def main():
     file_name = 'phone.csv'
@@ -69,6 +20,7 @@ def main():
         print("1. Вывести данные")
         print("2. Сохранить данные")
         print("3. Поиск по характеристике")
+        print("4. Копировать данные из одного файла в другой")
         print("q. Выйти")
 
         command = input("Введите номер команды: ")
@@ -84,6 +36,14 @@ def main():
             write_file(file_name, get_info())
         elif command == '3':
             pass
+        elif command == '4':
+            source_file = input("Введите имя файла, из которого нужно скопировать данные: ")
+            destination_file = input("Введите имя файла, в который нужно скопировать данные: ")
+
+            if exists(source_file) and exists(destination_file):
+                copy_entry(source_file, destination_file)
+            else:
+                print("Один из файлов не существует. Убедитесь, что оба файла существуют.")
         elif command.lower() == 'q':
             break
         else:
